53ee761f34dc178e153de480afefb17a90794320
Carloss32 <81336659+Carloss32@users.noreply.github.com>
2021-05-02 22:40:10 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -1,25 +1,19 @@
-
 // import exec module from child_process to run shell command
 // spawn was recommended for long output, but md5sum is short, so exec is ok
 const { exec } = require("child_process");
 
 exports.handler = (event, handler, callback) => {
 
-   
-   
+ //var input = "test"
+    var input = event.queryStringParameters.input.trim();
 
     // command to compute md5 of provided input
     // md5sum requires stdin, so this echos and pipes the value to md5sum
     // added sed to remove the trailing " -" that md5sum adds to output
     // added -n because echo includes a newline, causing md5sum to be wrong
     var cmd = 'echo -n "' + input + '" | md5sum | sed "s/ -//"';
-
-       //var input = "test"
-    var input = event.queryStringParameters.input.trim();
-       //var input = "test"
-    var input = event.queryStringParameters.input.trim();
    
-value = "teststring"
+   value = "teststring"
 
 if (!/^[a-z0-9 .-]*$/i.test(value)) {
         console.log('Bad input for ' + input);
@@ -28,8 +22,17 @@ if (!/^[a-z0-9 .-]*$/i.test(value)) {
                 body: "Please provide only letters, numbers, periods, dashes, and spaces"
         });
 }
+   
+    // execute the full command
+    exec(cmd, (error, stdout, stderr) => {
 
-
+        // there was an error, so return the error message
+        if (error) {
+            callback(null, {
+                statusCode: 400,
+                body: error
+            });
+        }
 
         // no lambda error, but the command failed, so return the error message
         if (stderr) {
