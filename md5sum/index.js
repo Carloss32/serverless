5ee43709f5d31f9198492b63dc3da17444d76ed2
Carloss32 <81336659+Carloss32@users.noreply.github.com>
2021-05-02 23:05:24 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -1,13 +1,4 @@
-// import exec module from child_process to run shell command
-// spawn was recommended for long output, but md5sum is short, so exec is ok
-const { exec } = require("child_process");
-
-exports.handler = (event, handler, callback) => {
-
-    //var input = "test"
-    var input = event.queryStringParameters.input.trim();
- 
-   value = "teststring"
+value = "teststring"
 
 if (!/^[a-z0-9 .-]*$/i.test(value)) {
         console.log('Bad input for ' + input);
@@ -16,6 +7,15 @@ if (!/^[a-z0-9 .-]*$/i.test(value)) {
                 body: "Please provide only letters, numbers, periods, dashes, and spaces"
         });
 }
+
+// import exec module from child_process to run shell command
+// spawn was recommended for long output, but md5sum is short, so exec is ok
+const { exec } = require("child_process");
+
+exports.handler = (event, handler, callback) => {
+
+    //var input = "test"
+    var input = event.queryStringParameters.input.trim();
 
     // command to compute md5 of provided input
     // md5sum requires stdin, so this echos and pipes the value to md5sum
